8a8a4fbf4f3716595a7097ccefa6467720be8a76
Oscar-G-Mojica-Barrera <oscar.mojica121@est.unanleon.edu.ni>
2024-08-08 19:29:14 -0600

    Add files via upload

---
+++ leer.rb
@@ -0,0 +1,8 @@
+#puts "Ingrese su primer nombre"
+#nombre = gets
+#puts "Bienvenido "+ nombre + "disfrute! "
+
+#Utiliza el metodo chomp para leer el nombre
+puts "Ingrese su primer nombre"
+nombre = gets.chomp
+puts "Bienvenido #{nombre} disfrute! "
